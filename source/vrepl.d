c80074e4a9723a183c0ec157d3dca24e854d3f2c
Zhao Puming <zhaopuming@gmail.com>
2016-04-08 22:36:50 +0800

    add signal treatment

--- source/vrepl.d
+++ source/vrepl.d
@@ -1,3 +1,9 @@
+/**
+ * Reference: 
+ * https://github.com/antirez/linenoise
+ * http://invisible-island.net/xterm/ctlseqs/ctlseqs.html
+ * http://www.3waylabs.com/nw/WWW/products/wizcon/vt220.html
+ **/
 module vrepl;
 
 import std.stdio: write, writeln;
@@ -7,6 +13,8 @@ import std.process;
 
 alias StringSet = HashSet!string;
 
+enum ES_CLEAR = "\x1b[H\x1b[2J";
+enum ES_BEEP = "\x07";
 enum Mode { SHELL, LINE, MLINE, EDIT }
 
 class State {
@@ -24,7 +32,7 @@ class Config {
             LINE: ">",
             MLINE: ">>",
             EDIT: "*>"
-                ];
+        ];
 
         quits.add("quit", "bye", "exit");
     }
@@ -72,14 +80,27 @@ class Vrepl {
         import std.string: chomp;
         import std.stdio: stdin, readln;
 
+        import core.stdc.signal;
+        signal(21, SIG_IGN);
+        signal(22, SIG_IGN);
+        signal(23, SIG_IGN);
         string line;
-        with (Mode) switch (state.mode) {
-            case LINE:
+        prompt();
+        while ((line = stdin.readln) !is null) {
+
+            line = line.chomp;
+            if (isQuit(line)) return;
+            if (!checkMode(line)) {
                 prompt();
-                while ((line = stdin.readln) !is null) {
-                    line = line.chomp;
-                    if (isQuit(line)) return;
-                    if (checkMode(line)) {
+                continue;
+            }
+            with (Mode) switch (state.mode) {
+                case LINE:
+                    if (line == "clear") {
+                        writeln(ES_CLEAR);
+                    } else if (line == "beep") {
+                        writeln(ES_BEEP);
+                    } else {
                         if (config.onInput != null) {
                             config.onInput(line);
                         } else {
@@ -87,41 +108,42 @@ class Vrepl {
                         }
                     }
                     prompt();
-                }
-                quit();
-                break;
-            case SHELL:
-                prompt();
-                while ((line = stdin.readln) !is null) {
-                    line = line.chomp;
-                    if (isQuit(line)) return;
-                    if (checkMode(line)) {
-                        {
-                            import std.process;
-                            /*
-                            pipes = pipeProcess(["/bin/bash", "-i", "-l"], Redirect.all);
-                            scope(exit) tryWait(pipes.pid);
-                            pipes.stdin.writeln(line);
-//                            pipes.stdin.writeln("\r\n");
-                            pipes.stdin.close();
-                            import core.stdc.stdio;
-                            foreach (buf; pipes.stdout.byLine) {
-                                writeln(buf);
-                            }
-                            */
-
-                            wait(spawnProcess(["/bin/bash", "-i", "-c", line]));
-                            //auto r = executeShell("/bin/bash -c " ~ line);
+                    quit();
+                    break;
+                case SHELL:
+                    writeln("shellll");
+                    writeln("shell");
+                    {
+                        import std.process;
+                        /*
+                        pipes = pipeProcess(["/bin/bash", "-i", "-l"], Redirect.all);
+                        scope(exit) tryWait(pipes.pid);
+                        pipes.stdin.writeln(line);
+                        pipes.stdin.writeln("\r\n");
+                        //pipes.stdin.close();
+                        import core.stdc.stdio;
+                        foreach (buf; pipes.stdout.byLine) {
+                            writeln(buf);
                         }
+                        */
+
+                        // wait(spawnProcess(["/bin/bash", "-i", "-c", line]));
+                           auto r = executeShell("/bin/bash -i -c '" ~ line ~ "'");
+                           if (r.status !=0) {
+                           writeln("error executing:", line);
+                           } else {
+                           writeln(r.output);
+                           }
+
                     }
                     prompt();
-                }
-                quit();
-                break;
-            default:
-                break;
+                    break;
+                default:
+                    break;
+            }
+            quit();
+
         }
-
     }
-}
 
+}
